b5ffbeda261b55edce39415c9bcb35c714c77a19
John George <joecracko@gmail.com>
2015-07-24 15:32:31 -0400

    Making Coaches page work with back-end data.

--- bower_components/app.js
+++ bower_components/app.js
@@ -48,6 +48,12 @@ app.config(function($routeProvider, $locationProvider){
 	$locationProvider.html5Mode(true);
 });
 
+app.factory('Coaches', function ($resource) {
+  return $resource('/api/coaches/:id', { id: '@id' }, {
+    'update': { method: 'PUT' }
+  });
+});
+
 app.controller('HomeController', function($scope){
 
 });
@@ -60,8 +66,25 @@ app.controller('PrivateLessonsController', function($scope){
 
 });
 
-app.controller('CoachesController', function($scope){
+app.controller('CoachesController', function($scope, Coaches, $http){
+	$scope.coaches = Coaches.query();
+	var i = 1;
 
+	// $scope.selected = {index:0};
+		
+	// $scope.updateSelectedCoach = function(index){
+	// 	$scope.selected.index = index;
+	// };
+	
+	// $http.get('/api/coaches')
+	// .success(function(data, status, headers, config){
+	// 	$scope.coaches = data;
+	// })
+	// .error(function(data, status, headers, config){
+	// 	$scope.coaches = [];
+	// });
+	
+	// var i = 10;
 });
 
 app.controller('ContactController', function($scope){
--- server.js
+++ server.js
@@ -2,8 +2,43 @@ var express = require('express'),
 			bodyParser = require('body-parser'),
 			app = express();
 
+var id = 2;
+var data = {
+	1: { id: 1, firstName: 'John' , lastname: 'George', details: 'Teh best coche evar'},
+	2: { id: 2, firstName: 'Joe', lastname: 'Recla', details: 'Teh secund best coche'}
+};
+
 app.use(bodyParser.json());
 app.use(express.static('./bower_components'));
+
+app.route('/api/coaches')
+	.get(function(req, res){
+		console.log(Object.keys(data).map(function(key){
+			return data[key];
+		}));
+		res.json(Object.keys(data).map(function(key){
+			return data[key];
+		}));
+	})
+	.post(function(req, res){
+		var record = req.body;
+		record.id = ++id;
+		data[record.id] = record;
+		res.json(record);
+	});
+
+app.route('/api/coaches/:id')
+	.get(function(req, res){
+		res.json(data[req.params.id]);
+	})
+	.put(function(req, res){
+		data[req.params.id] = req.body;
+		res.json(req.body);
+	})
+	.delete(function(req, res){
+		delete data[req.params.id];
+		res.json(null);
+	});
 
 app.get('*', function(req, res){
 	res.sendFile(__dirname + '/bower_components/index.html');
