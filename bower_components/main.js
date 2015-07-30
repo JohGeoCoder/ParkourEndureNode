02ef8c80c518c2156e1b546bfc5e50b90e3fb412
John George <joecracko@gmail.com>
2015-07-30 10:30:11 -0400

    The nav links are properly highlighted in production.

--- bower_components/main.js
+++ bower_components/main.js
@@ -4,6 +4,10 @@
 			$('.off-canvas-list li').on('click.toggleCanvas', function(){
 				$(".exit-off-canvas").click();
 			});
+
+			$('.link').on('click', function(){
+				$('.top-bar-section li').removeClass('active');
+			});
 		};
 		
 		return{
