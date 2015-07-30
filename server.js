83e5de861c9c87c7f20585a36d506010c299b170
John George <joecracko@gmail.com>
2015-07-30 17:00:37 -0400

    Updated coach information for John George

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express'),
 
 var coachId = 1;
 var coachData = {
-	1: { id: 1, firstName: 'John' , lastName: 'George', imageUrl: '../img/pkCoachJohn.png', details: 'Teh best coche evar'},
+	1: { id: 1, firstName: 'John' , lastName: 'George', imageUrl: '../img/pkCoachJohn.png', details: 'John began practicing parkour in 2005 when parkour coaching in the United States was almost non-existent. He began coaching in 2009 during his time at Bloomsburg University of Pennsylvania, and earned his Level 1 ADAPT coaching qualification in 2011. In 2015 John took the intensive 5-day Level 2 ADAPT coaching qualification, and is actively training for the Level 2 assessments.'},
 };
 
 var carouselItemId = 3;
