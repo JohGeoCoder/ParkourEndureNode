11f7596690b16dccea6e434a908cdc987d6c9d7c
John George <joecracko@gmail.com>
2015-07-31 14:10:42 -0400

    Removing unnecessary javascript.

--- bower_components/main.js
+++ bower_components/main.js
@@ -4,10 +4,6 @@
 			$('.off-canvas-list li').on('click.toggleCanvas', function(){
 				$(".exit-off-canvas").click();
 			});
-
-			$('.link').on('click', function(){
-				$('.top-bar-section li').removeClass('active');
-			});
 		};
 		
 		return{
