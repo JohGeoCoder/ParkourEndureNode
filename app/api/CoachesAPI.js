bbb79ff52a3c20a386aacaa024f88809b5639135
Joecracko <joecracko@gmail.com>
2016-04-01 22:07:32 -0400

    Moved all Coaches functionality to a business layer.

--- app/api/CoachesAPI.js
+++ app/api/CoachesAPI.js
@@ -1,61 +1,17 @@
-var Coach = require('../models/coach');
 var CoachesBL = require('../business_layer/CoachesBL.js');
 
 module.exports = function(app){
 
 	app.get('/api/coaches', function(req, res){
-		CoachesBL.getAllCoaches(req, res);
+		CoachesBL.getAll(req, res);
 	});
 
 	app.put('/api/coaches', function(req, res){
-		var newCoach = new Coach({
-			firstName : "First",
-			lastName : "Last",
-			imageUrl : "img/pkCoachJohn.png",
-			details : "details"
-		});
-		newCoach.save(function(err){
-			if(err){
-				res.json({
-					'error' : err.message
-				});
-			}
-
-			res.json(newCoach);
-		});
+		CoachesBL.create(req, res);
 	});
 
 	app.post('/api/coaches', function(req, res){
-		Coach.find({'_id' : req.body['objectId']}, function(err, coaches){
-			if(err){
-				res.json({
-					'error' : err.message
-				});
-				return;
-			}
-
-			var coachToUpdate = coaches[0];
-
-			if(coachToUpdate){
-				coachToUpdate.firstName = req.body['firstName'];
-				coachToUpdate.lastName = req.body['lastName'];
-				coachToUpdate.imageUrl = req.body['imageUrl'];
-				coachToUpdate.details = req.body['details'];
-
-				coachToUpdate.save(function(err){
-					if(err){
-						res.json({
-							'error' : err.message
-						});
-						return;
-					}
-
-					res.json({
-						'success' : true
-					});
-				});
-			}
-		});
+		CoachesBL.update(req, res);
 	});
 
 }
