a2eceb57ba05c82ebc364ee5011b51372aec31df
Joecracko <joecracko@gmail.com>
2016-04-05 20:28:35 -0400

    Added Winston Logging to the Packages

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var port     = process.env.PORT || 80;
 var mongoose = require('mongoose');
 var passport = require('passport');
 var flash    = require('connect-flash');
+var winston  = require('winston');
 
 var morgan       = require('morgan');
 var cookieParser = require('cookie-parser');
