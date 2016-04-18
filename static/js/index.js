db71a28bebd2dc2c116429bd02cdad4518a9d445
Joecracko <joecracko@gmail.com>
2016-04-17 20:15:07 -0400

    Moved analytics to a file that can be ignored.

--- static/js/index.js
+++ static/js/index.js
@@ -2,24 +2,11 @@
 	var header = function(){
 		var init = function(){
 			$('.close-modal').on('click', modalClose);
-
 			$("#spring2016ClassesModal").foundation('reveal', 'open');
-
-			//startAnalytics();
 		};
 
 		var modalClose = function(){
 			FoundationHelper.closeModals();
-		};
-
-		var startAnalytics = function(){
-		    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-		    ga('create', 'UA-68185666-1', 'auto');
-		    ga('send', 'pageview');
 		};
 
 		return {
