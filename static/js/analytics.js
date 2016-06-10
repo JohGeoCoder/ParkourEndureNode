78eb47e81aab32d1e262062676fd063a81d8f1d9
Joecracko <joecracko@gmail.com>
2016-06-10 08:05:36 -0400

    Added the They Said social media.

--- static/js/analytics.js
+++ static/js/analytics.js
@@ -12,6 +12,15 @@
 
 		    ga('create', 'UA-68185666-1', 'auto');
 		    ga('send', 'pageview');
+
+		    !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+		    n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+		    n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+		    t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+		    document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
+		    fbq('init', '1607359499575194');
+		    fbq('track', "PageView");
 		};
 
 		return {
